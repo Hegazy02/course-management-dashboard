85970fc4b776aea546003d88696752c23d948863
Hegazy02 <Abdelrhmanhegazy44@gmail.com>
2026-07-12 19:13:35 +0300

    style: change toast style

--- src/app/shared/components/toast/toast.component.ts
+++ src/app/shared/components/toast/toast.component.ts
@@ -1,11 +1,13 @@
 import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { ToastService } from '../../../core/services/toast.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-toast',
   standalone: true,
   templateUrl: './toast.component.html',
   styleUrl: './toast.component.css',
+  imports: [NgClass],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ToastComponent {
