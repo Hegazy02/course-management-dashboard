6d34be52e46e130c32add3c168b0309ce7488d55
Hegazy02 <Abdelrhmanhegazy44@gmail.com>
2026-07-12 15:05:43 +0300

    feat: create reusable table

---
+++ src/app/shared/components/general-table/general-table.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { GeneralTableComponent } from './general-table.component';
+
+describe('GeneralTableComponent', () => {
+  let component: GeneralTableComponent;
+  let fixture: ComponentFixture<GeneralTableComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [GeneralTableComponent],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(GeneralTableComponent);
+    component = fixture.componentInstance;
+    await fixture.whenStable();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/shared/components/general-table/table-column.model.ts
@@ -0,0 +1,11 @@
+export interface TableColumn<T> {
+  field: Extract<keyof T, string>;
+  header: string;
+  type?: 'text' | 'date' | 'number' | 'boolean' | 'tag';
+  clickable?: boolean;
+  sortable?: boolean;
+  filterable?: boolean;
+  width?: string;
+  format?: string;
+  severity?: (row: T) => string;
+}
